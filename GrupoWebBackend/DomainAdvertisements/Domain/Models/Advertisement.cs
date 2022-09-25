ab602d76c2059c7c5093296eba69e1fb087452f1
Fulgencio Marx Vilca Morales <56830946+BLOOOP788@users.noreply.github.com>
2022-09-25 18:20:50 -0500

    Update Advertisement.cs

--- GrupoWebBackend/DomainAdvertisements/Domain/Models/Advertisement.cs
+++ GrupoWebBackend/DomainAdvertisements/Domain/Models/Advertisement.cs
@@ -4,15 +4,7 @@ namespace GrupoWebBackend.DomainAdvertisements.Domain.Models
 {
     public class Advertisement
     {
-        public int Id { get; set; }
-        public string DateTime { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public float Discount { get; set; }
-        public string UrlToImage { get; set; }
-        public bool Promoted { get; set; }
-        public User User { get; set; }
-        public int UserId { get; set; } 
+  
         
       
     }
