62123e40c23104f6cc1ea7d5df749fab35ca3746
Fulgencio Marx Vilca Morales <56830946+BLOOOP788@users.noreply.github.com>
2022-09-25 18:23:45 -0500

    Adding class atributes

--- GrupoWebBackend/DomainPets/Domain/Models/Pet.cs
+++ GrupoWebBackend/DomainPets/Domain/Models/Pet.cs
@@ -7,6 +7,20 @@ namespace GrupoWebBackend.DomainPets.Domain.Models
 {
     public class Pet
     {
-        
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Attention { get; set; }
+        public int Age { get; set; }
+        public string Race { get; set; }
+        public bool IsAdopted { get; set; }
+        public bool IsPublished { get; set; }
+        public string Gender { get; set; }
+        public string UrlToImage { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        [AllowNull]
+        public int? PublicationId { get; set; }
+        public Publication Publication { get; set; }
     }
 }
